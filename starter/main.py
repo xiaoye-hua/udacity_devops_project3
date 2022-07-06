2809691a3f06290c30d57bfbbd23ac60fed6be87
xiaoye-hua <huag@kth.se>
2022-07-06 21:03:10 +0200

    change code

--- starter/main.py
+++ starter/main.py
@@ -1,1 +1,14 @@
 # Put the code for your API here.
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Value(BaseModel):
+    value: int
+
+
+@app.post("/{path}")
+async def exercise_function(path: int, query: int, body: Value):
+    return {"path": path, "query": query, "body": body}
