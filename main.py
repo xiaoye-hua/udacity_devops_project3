72c22e7fc6408fd29a595f87113f6246c9e94f52
xiaoye_hua <huag@kth.se>
2022-07-17 11:06:30 +0200

    change code for debug

--- main.py
+++ main.py
@@ -1,58 +1,24 @@
-from typing import Union
-import joblib
 from fastapi import FastAPI
-# BaseModel from Pydantic is used to define data objects.
-from pydantic import BaseModel, Field
-import uvicorn
-import pandas as pd
-
-
-# Declare the data object with its components and their type.
-class Feature(BaseModel):
-    # education_num: int=Field(default=9, alias='education-num')
-    # capital_loss: int=Field(default=0, alias='capital-loss')
-    a: int=3
-    b: int=4
-
-
-class TaggedItem(BaseModel):
-    name: str
-    tags: Union[str, list]
-    item_id: int
 
 app = FastAPI()
 
-model_path = 'model/model.pkl'
-model = joblib.load(model_path)
-
 
 @app.get("/")
-async def say_hello():
-    return {"greeting": "Hello World!"}
+def read_root():
+    return {"200": "Welcome To Heroku"}
 
 
-@app.post("/inference/")
-async def get_inference(
-        # feature: Feature
-        education_num: int=9,
-        capital_loss: int=0
-                        ):
-    feature ={'a':education_num, 'b':capital_loss}
-    for key, value in feature.items():
-        feature[key] = [value]
-    res = model.predict(pd.DataFrame(feature)).tolist()[0]
-    return res
-    # return feature
-# A GET that in this case just returns the item_id we pass,
-# but a future iteration may link the item_id here to the one we defined in our TaggedItem.
-# @app.get("/items/")
-# async def get_items(item_id: int=5, count: int = 1):
-#     return {"fetch": f"Fetched {count} of {item_id}"}
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
 
-# This allows sending of data (our TaggedItem) via POST to the API.
-# @app.get("/items/")
-# async def create_item(item: TaggedItem):
-#     return item
 
-if __name__ == "__main__":
-    uvicorn.run('main:app', debug=True, reload=True)
+@app.get("/fizz_buzz/{num}")
+def read_item(num: int):
+    # https: // ja.wikipedia.org / wiki / Fizz_Buzz
+    if not num % 15:
+        return {num: "Fizz Buzz"}
+    elif not num % 5 or not num % 3:
+        return {num: 'Fizz' if not num % 3 else 'Buzz'}
+    else:
+        return {num: 'Stay Silent'}
